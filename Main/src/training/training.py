44855f3259dd98f32c342e4e96df362c6b678aed
professorparad <sparshochakraborty123@gmail.com>
2026-05-24 15:10:10 +0530

    deleted:    Main/src/decoder 	renamed:    Main/src/decoder /__init__.py -> Main/src/decoder/__init__.py 	new file:   Main/src/decoder/__pycache__/__init__.cpython-312.pyc 	new file:   Main/src/decoder/__pycache__/patch_decoder.cpython-312.pyc 	renamed:    Main/src/decoder /patchdecoder.py -> Main/src/decoder/patch_decoder.py 	new file:   Main/src/preprocessing/__pycache__/image_loader.cpython-312.pyc 	new file:   Main/src/preprocessing/__pycache__/patching.cpython-312.pyc 	new file:   Main/src/preprocessing/__pycache__/positional_encoding.cpython-312.pyc 	new file:   Main/src/tensornetworks/__pycache__/__init__.cpython-312.pyc 	new file:   Main/src/tensornetworks/__pycache__/mps_features.cpython-312.pyc 	new file:   Main/src/training/__pycache__/__init__.cpython-312.pyc 	new file:   Main/src/training/__pycache__/training.cpython-312.pyc 	modified:   Main/src/training/training.py

--- Main/src/training/training.py
+++ Main/src/training/training.py
@@ -14,4 +14,33 @@ def build_dataset():
     image = load_image_grayscale(img_path)
     patches, positions = extract_patches(image , patchsize=64)
     features = np.array([extract_mps_features(p) for p in patches])
-    
+    features = torch.tensor(features,dtype = np.float32)
+    features = (features-features.mean(dim=0))/(features.std(dim=0)+1e-8)
+    postions = sinusodial_encoding(positions , d_model = 8)
+    targets = torch.tensor(patches , dtype = np.float32).unsqueeze(1)
+    return(features.to(device) , positions.to(device) , targets.to(device))
+
+def train():
+    features , positions , targets = build_dataset()
+    model = QuantumModel(feature_dim=features.shape[1] , positional_dim= positions.shape[1]).to(device)
+    decoder = PatchDecoder(observable_dim = observable_dim , positional_dim =positions.shape[1] ,patch_size = 64 ).to(device)
+    optimizer = optim.Adam(list(model.parameters())+ list(decoder.parameters()) , lr = 0.003 )
+    for step in range(120):
+        model.train()
+        optimizer.zero_grad()
+        observables , energies = model(features , positions)
+        output = decoder(observables , positions)
+        reconstruction_loss = torch.mean((output -targets )**2)
+        energy_loss = torch.mean(energies)
+        loss = (reconstruction_loss+ 0.01*energy_loss)
+        loss.backward()
+        optimizer.step()
+        if step%20 == 0 :
+            print(
+                f"step:{step:> 4d}|"
+                f"loss :{loss.item():.6f} |"
+                f"recon : {reconstruction_loss.item():.6f}|"
+                f"Energy:{energy_loss.item():.6f}"
+            )
+    return model , decoder 
+
